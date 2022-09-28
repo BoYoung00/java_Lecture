3668355defe94ae5a868e6de7cdccbd889e5db84
BoYoung00 <qhdud20@naver.com>
2022-09-28 23:21:56 +0900

    9/21, 9/28 자바 강의

---
+++ java0921_3.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class java0921_3 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.printf("점수 입력 : ");
+        int score = in.nextInt();
+        String grade;
+
+        switch (score / 10) {
+            case 10 : grade = "A"; break;
+            case 9 : grade = "A"; break;
+            case 8 : grade = "B"; break;
+            case 7 : grade = "C"; break;
+            case 6 : grade = "D"; break;
+            default : grade = "F";
+        }
+        System.out.printf("등급 : %s\n", grade);
+    }
+}
---
+++ java0928_3.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class java0928_3 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.printf("문자를 입력하세요 : ");
+        String a = in.nextLine();
+
+
+        for (int i = a.length()-1; i >= 0; i--) {
+            System.out.printf("%c", a.charAt(i));
+        }
+    }
+}

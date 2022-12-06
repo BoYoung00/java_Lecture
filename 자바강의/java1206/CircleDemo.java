9d6171d2524252f3bc4169728a209a93b3941be9
BoYoung00 <qhdud20@naver.com>
2022-12-06 15:04:53 +0900

    자바 1206 강의

---
+++ 자바강의/java1206/CircleDemo.java
@@ -0,0 +1,42 @@
+package 자바강의.java1206;
+
+class Circle {
+    int radius;
+    Circle() {
+        radius = 1;
+    }
+    Circle(int radius) {
+        this.radius = radius;
+    }
+    void show() {
+        System.out.printf("반지름 : %d\n", radius);
+    }
+}
+class ColorCircle extends Circle {
+    String color;
+    ColorCircle() {
+        super();
+        this.color = "yellow";
+    }
+    ColorCircle(String s) {
+        super();
+        this.color = s;
+    }
+    ColorCircle(int n, String s) {
+        super(n);
+        this.color = s;
+    }
+    void show() {
+        System.out.printf("반지름 : %d, 색상 : %s\n", radius, color);
+    }
+}
+public class CircleDemo {
+    public static void main(String[] args) {
+        ColorCircle c1 = new ColorCircle();
+        c1.show();
+        ColorCircle c2 = new ColorCircle("red");
+        c2.show();
+        ColorCircle c3 = new ColorCircle(5, "blue");
+        c3.show();
+    }
+}

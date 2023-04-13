dd2ddc712cf6d8688ce5660bbfad114a06cb5d39
BoYoung00 <qhdud20@naver.com>
2023-04-13 16:51:44 +0900

    4/13 자바 공부 Format

---
+++ 자바공부2023/DecimalFormatTest.java
@@ -0,0 +1,23 @@
+package 자바공부2023;
+
+import java.text.DecimalFormat;
+
+public class DecimalFormatTest {
+    public static void main(String[] args) {
+        DecimalFormat df = new DecimalFormat("#,###.##");
+        DecimalFormat df2 = new DecimalFormat("#.###E0");
+
+        try {
+            Number num = df.parse("1,234,567.89");
+            System.out.println("1,234,567.89" + " -> ");
+
+            double d = num.doubleValue();
+            System.out.print(d + " -> ");
+
+            System.out.println(df2.format(num));
+        } catch (Exception e) {
+
+        }
+    }
+
+}

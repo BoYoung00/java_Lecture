9dc6882d4c3389dc17f3ff10cc46aa49641feef9
BoYoung00 <101164065+BoYoung00@users.noreply.github.com>
2023-05-04 19:19:57 +0900

    5/4 자바과제 진행중

--- 자바과제2023/Operation.java
+++ 자바과제2023/Operation.java
@@ -1,39 +1,56 @@
 package 자바과제2023;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 interface IOperation {
     void modify(); //수식입력
-    int[] operatorLocation(); //연산자 위치 반환
+    void operatorLocation(); //연산자 위치 반환
     void result(); //결과값 반환
 }
 
 class Operation implements IOperation {
-    String input; //수식입력
     StringBuilder anOperand = new StringBuilder(); //피연산자(숫자)
     StringBuilder arithmetic = new StringBuilder(); //연산자(문자)
+    String input; //수식 입력 받기
+    String[] str; //수식 한 문자씩 자르기
     Scanner in = new Scanner(System.in);
 
     @Override
     public void modify() { //수식입력
         System.out.print("수식을 입력하세요 : ");
-        input = in.nextLine();
-
-        for (int i = 0; i < input.length(); i++) {
-            if (Character.isDigit(input.charAt(i))){ //문자가 숫자면
-                anOperand.append(input.charAt(i));
-            } else {
-                arithmetic.append(input.charAt(i));
-            }
-        }
-        System.out.println(anOperand);
-        System.out.println(arithmetic);
+        input = in.nextLine(); // 수식 입력
+        str = input.split(""); //문자 하나씩 나누기
     }
 
     @Override
-    public int[] operatorLocation() { //연산자 위치 반환
-        return null;
+    public void operatorLocation() { //연산자 위치 반환
+        int cutIndex = 0;
+
+        for (int i = 0; i < input.length(); i++){
+            // 문자 중에 연산자를 찾으면 그 전부터 지금 문자까지 잘라서 저장
+            if(str[i].equals("*") || str[i].equals("/") || str[i].equals("+") || str[i].equals("-")) {
+//                double oper = Double.parseDouble(input.substring(cutIndex, i));
+                String oper = input.substring(cutIndex, i);
+                anOperand.append(oper); //피연산자
+                cutIndex = i + 1;
+            }
+            if( (i+1)==input.length()){
+                System.out.println(input.substring(cutIndex));
+            }
+        }
+
+//        for (int i = 0; i < input.length(); i++) {
+//            if (Character.isDigit(input.charAt(i))){ //문자가 숫자면
+//                anOperand.append(input.charAt(i));
+//            } else {
+//                arithmetic.append(input.charAt(i));
+//            }
+//        }
+//        System.out.println(anOperand);
+//        System.out.println(arithmetic);
     }
 
     @Override
@@ -47,8 +64,6 @@ class OperationDemo {
 //        Operation oper = new Operation();
 //        oper.modify();
 
-        String str = "abc#def#ghi";
-        String[] splitStr = str.split("#");
-        System.out.println(Arrays.stream(splitStr).toList());
+
     }
 }

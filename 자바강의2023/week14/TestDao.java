ed81587b2399b485654489421a8665a94a6fd8c5
BoYoung00 <101164065+BoYoung00@users.noreply.github.com>
2023-06-14 01:25:02 +0900

    6/14 자바 기말시험 대비

--- 자바강의2023/week14/TestDao.java
+++ 자바강의2023/week14/TestDao.java
@@ -2,14 +2,20 @@ package 자바강의2023.week14;
 
 import java.sql.*;
 
+// exexute : 쿼리이면 true 아니면 false
+// -> 반환 값이 true라면 getResultSet 메서드 사용 가능
+// executeUpdate : 추가, 삭제, 수정하는 sql문 실행
+// -> 반환 값은 SQL문 실행에 영향 받은 행수 반환 (오류 시 -1)
+// executeQuery : select문에서만 실행
+// -> 반환 값은 ResultSet
 public class TestDao {
 	
-	Connection conn = null;
-	Statement stmt = null;
-	ResultSet rs = null;
-	PreparedStatement pstmt = null;
+	Connection conn = null; // DB 연결용
+	Statement stmt = null; // SQL 구문을 실행하는 역할
+	ResultSet rs = null; // 값 넣는 곳
+	PreparedStatement pstmt = null; // Statement 기능 향상 (? 쓸 수 있게 해줌)
 	
-	public TestDao() throws ClassNotFoundException, SQLException {
+	public TestDao() throws ClassNotFoundException, SQLException { // 드라이버 로드
 		Class.forName("com.mysql.cj.jdbc.Driver");
 		conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/2-B"
 				+ "?characterEncoding=UTF-8&serverTimezone=UTC&useSSL=false", "root", "root1234");
@@ -18,7 +24,7 @@ public class TestDao {
 		
 	}
 	
-	public void createTable() throws SQLException {
+	public void createTable() throws SQLException { // 테이블 생성
 		stmt.executeUpdate("create table JDBCTest(name varchar(20), age int)");
 		System.out.println("JDBCTest 테이블 생성 완료");
 		String sql = null;
@@ -30,7 +36,7 @@ public class TestDao {
 		}
 	}
 	
-	public void selectAll() throws SQLException {
+	public void selectAll() throws SQLException { // 테이블 전체 출력
 		rs = stmt.executeQuery("select name, age from JDBCTest");
 		while (rs.next()) {
 			System.out.println("name : " + rs.getString(1) + " age : " + rs.getInt("age"));
@@ -38,7 +44,7 @@ public class TestDao {
 		}
 	}
 	
-	public void selectUsingExecute(String sql) throws SQLException {
+	public void selectUsingExecute(String sql) throws SQLException { // sql문 받아서 출력
 		boolean isResult = stmt.execute(sql); // sql문이 맞는지 확인
 		if (isResult) { //True : ResultSet 객체를 반환
 			rs = stmt.getResultSet(); // 테이블 값 가져오기 한 행씩
@@ -48,7 +54,7 @@ public class TestDao {
 			System.out.println("rowCount : " + stmt.getUpdateCount());
 	}
 	
-	public void usingPreStatment(String name, int age) throws SQLException {
+	public void usingPreStatment(String name, int age) throws SQLException { // 일치하는 값 출력
 		pstmt = conn.prepareStatement("select name, age from JDBCTest where name=? and age >= ?");
 		pstmt.setString(1, name);
 		pstmt.setInt(2, age);
@@ -59,16 +65,18 @@ public class TestDao {
 	}
 	
 	public void usingTansaction() {
+		// 트랜잭션 : 그룹으로 처리해야하느 명령문을 모아놓은 작업 단위
+		// -> 하나라도 오류나면 전체 명령문 취소
 		try {
 			conn.setAutoCommit(false); // 트랜젝션의 시작
 			stmt.executeUpdate("Insert Into JDBCTest (name, age) values('홍길동10', 20)");
 			stmt.executeUpdate("Insert Into JDBCTest (name, age) values('임꺽정11', 21)"); // 일부러 오류
-			conn.commit(); // 트랜젝션의 범위 (만나기 전까지 트렌젝션)
+			conn.commit(); // 트랜젝션의 범위 (만나기 전까지 트렌젝션) commit : 모든 명령문이 성공했을 때
 			selectAll();
 		} catch (Exception e) {
 			try {
 				// 트랜젝션의 범위 (만나기 전까지 트렌젝션)
-				conn.rollback(); // 정상적인 코드까지 취소시킴
+				conn.rollback(); // 하나라도 오류뜨면 정상적인 코드까지 취소시킴
 				selectAll();
 			} catch (Exception e2) {} 
 		}
